0e5d8a66056739c1346e85a61fe87ed3cd9513fd
noolua <noolua@gmail.com>
2023-02-11 21:15:19 +0800

    recoding st7302 init

--- Adafruit_ST7302.cpp
+++ Adafruit_ST7302.cpp
@@ -1,6 +1,35 @@
 #include "Adafruit_ST7302.h"
 
 
+static uint8_t st7302_init[] = {
+  0x01, 0x38,       // High Power Mode ON
+  0x02, 0xeb, 0x02, // NVM Load Enable with not be trigger by sleep Out.
+  0x02, 0xd7, 0x68, // NVM Load Control with Enable ID1/ID/ID3 Load, with Enable Source High/Low Voltage Load
+  0x02, 0xd1, 0x01, // Booster Enable switch, set enable
+  0x02, 0xc0, 0x80, // Gate Voltage Control
+  0x07, 0xc1, 0x28, 0x28, 0x28, 0x28, 0x14, 0x00,   // Source Voltage Control1: Source High Voltage set 5v, Gamma1 Voltage set 3.5v, Gamma2 Voltage set 1.5v
+  0x05, 0xc2, 0x00, 0x00, 0x00, 0x00, // Source Voltage Control2: Source Low Voltage set 0v
+  0x02, 0xcb, 0x14, // VCOMH Voltage Setting set 4v
+  0x0b, 0xb4, 0xe5, 0x77, 0xf1, 0xff, 0xff, 0x4f, 0xf1, 0xff, 0xff, 0x4f, // Update Period Gate EQ Control
+  0x01, 0x11,       // Sleep Out, This command turn off sleep mode.In this mode the DC/DC converter is enable, internal display oscillator is started, and panel scanning is started.
+  0x03, 0xc7, 0xa6, 0xe9, // OSC Enable: set enable
+  0x02, 0xb0, 0x40, // DutySetting: bug?? 0x64*4=400,  250/4 = 63.3 -> 64(0x40)
+  0x02, 0x36, 0x00, // Memory Data Access Control: Page Address Order:Top to Bottom, Column Address Order:Left to Right, Page/Column Order:Column Direction Mode, Line Address Order: LCD Refresh Top to Bottom, Data Order:Left to Right, Gate Scan Order: Data refresh Top to Bottom
+  0x02, 0x3a, 0x11, // Data Format Select: Data Up Down Switch->on, Bytes Per Pixel Select->3 write operations for 24-bit data
+  0x02, 0xb9, 0x23, // Source Setting: Clear RAM ON/OFF -> OFF
+  0x02, 0xb8, 0x09, // Panel Setting: Display Gate Scan Mode Select->Frame interval, Panel Layout Select ->One Line Interlace
+  0x03, 0x2a, 0x05, 0x36, // Column Address Set: 5-54 ?? (S61~S182)
+  0x03, 0x2b, 0x00, 0xc7, // Row Address Set: 0-199 (G1~G250)
+  0x02, 0xd0, 0x1f, // UNKNONW COMMAND
+  0x01, 0x29,       // Display On
+  0x02, 0xb9, 0xe3, // Source Setting: Clear RAM ON/OFF -> ON
+  0x02, 0xb9, 0x23, // Source Setting: Clear RAM ON/OFF -> OFF
+  0x02, 0x72, 0x00, // NKNONW COMMAND
+  0x01, 0x39,       // Low Power Mode ON
+  0x00              // END-FLAG
+};
+
+
 Adafruit_ST7302::Adafruit_ST7302(int8_t SID, int8_t SCLK, int8_t DC, int8_t RST, int8_t CS)
   :Adafruit_GFX(ST7302_WIDTH, ST7302_HEIGHT){
   _sid = SID;
@@ -31,71 +60,17 @@ void Adafruit_ST7302::begin(int32_t freq){
     Init LCD Driver
     REF: ST7302 DATASHEET
   */
-  _send_command(0x38);       // High Power Mode ON
-  _send_command(0xeb);       // NVM Load Enable with not be trigger by sleep Out.
-  _send_param(0x02);
-  _send_command(0xd7);       // NVM Load Control with Enable ID1/ID/ID3 Load, with Enable Source High/Low Voltage Load
-  _send_param(0x68);
-  _send_command(0xd1);       // Booster Enable switch, set enable
-  _send_param(0x01);
-  _send_command(0xc0);       // Gate Voltage Control
-  _send_param(0x80);
-  _send_command(0xc1);       // Source Voltage Control1: Source High Voltage set 5v, Gamma1 Voltage set 3.5v, Gamma2 Voltage set 1.5v
-  uint8_t svc1[] = {0x28,0x28,0x28,0x28,0x14,0x00};
-  _send_params(svc1, sizeof(svc1));
-  _send_command(0xc2);       // Source Voltage Control2: Source Low Voltage set 0v
-  uint8_t svc2[] = {0x00, 0x00, 0x00, 0x00};
-  _send_params(svc2, sizeof(svc2));
-  _send_command(0xcb);       // VCOMH Voltage Setting set 4v
-  _send_param(0x14);
-  _send_command(0xb4);       // Update Period Gate EQ Control
-  uint8_t upgec[] = {0xe5,0x77,0xf1,0xff,0xff,0x4f,0xf1,0xff,0xff,0x4f};
-  _send_params(upgec, sizeof(upgec));
-  _send_command(0x11);       // Sleep Out, This command turn off sleep mode.In this mode the DC/DC converter is enable, internal display oscillator is started, and panel scanning is started.
-  delay(100);
-
-  _send_command(0xc7);       // OSC Enable: set enable
-  _send_param(0xa6);
-  _send_param(0xe9);
-
-  _send_command(0xb0);       // DutySetting: bug?? 0x64*4=400,  250/4 = 63.3 -> 64(0x40)
-  _send_param(0x40);
-  _send_command(0x36);       // Memory Data Access Control: Page Address Order:Top to Bottom, Column Address Order:Left to Right, Page/Column Order:Column Direction Mode, Line Address Order: LCD Refresh Top to Bottom, Data Order:Left to Right, Gate Scan Order: Data refresh Top to Bottom
-  _send_param(0x00);
-  _send_command(0x3a);       // Data Format Select: Data Up Down Switch->on, Bytes Per Pixel Select->3 write operations for 24-bit data
-  _send_param(0x11);
-  _send_command(0xb9);       // Source Setting: Clear RAM ON/OFF -> OFF
-  _send_param(0x23);
-  _send_command(0xb8);       // Panel Setting: Display Gate Scan Mode Select->Frame interval, Panel Layout Select ->One Line Interlace
-  _send_param(0x09);
-  _send_command(0x2a);       // Column Address Set: 5-54 ?? (S61~S182)
-  _send_param(0x05);
-  _send_param(0x36);
-
-  _send_command(0x2b);       // Row Address Set: 0-199 (G1~G250)
-  _send_param(0x00);
-  _send_param(0xc7);
-  _send_command(0xD0);       // UNKNONW COMMAND
-  _send_param(0x1f);
-
-  _send_command(0x29);       // Display On
-  _send_command(0xb9);      // Source Setting: Clear RAM ON/OFF -> ON
-  _send_param(0xe3);
-  delay(100);      
-
-  _send_command(0xb9);       // Source Setting: Clear RAM ON/OFF -> OFF
-  _send_param(0x23);
-  _send_command(0x72);       // UNKNONW COMMAND
-  _send_param(0x00);
-  _send_command(0x39);       // Low Power Mode ON
-  _send_command(0x2a);       // Column Address Set: 0x19-0x23
-  _send_param(0x19);
-  _send_param(0x23);
-  _send_command(0x2b);       // Row Address Set: 0-124
-  _send_param(0x0);
-  _send_param(0x7c);
-  _send_command(0x2c);       // Write Frame Data    
-  delay(120);
+  int32_t cmd_pos = 0;
+  uint8_t cmd_sz = st7302_init[cmd_pos];
+  while(cmd_sz){
+    _send_command(st7302_init[cmd_pos + 1]);
+    if(cmd_sz > 1){
+      _send_params(&st7302_init[cmd_pos+2], cmd_sz - 1);
+    }
+    // next command
+    cmd_pos += (cmd_sz + 1);
+    cmd_sz = st7302_init[cmd_pos];
+  }
 }
 
 void Adafruit_ST7302::clearDisplay(){
